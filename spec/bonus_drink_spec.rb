07211e4ede2b10971cfcaef34ba4e1cb13c583ec
k.murahama <k.murahama@lexues.co.jp>
2015-04-03 06:51:58 +0900

    add testcase

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -2,4 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+  specify { expect(BonusDrink.total_count_for(101)).to eq 151 }
+
+  # wrong case
+  specify { expect(BonusDrink.total_count_for(2)).not_to eq 3 }
+  specify { expect(BonusDrink.total_count_for(5)).not_to eq 6 }
+  specify { expect(BonusDrink.total_count_for(10)).not_to eq 15 }
+  specify { expect(BonusDrink.total_count_for(99)).not_to eq 149 }
+  specify { expect(BonusDrink.total_count_for(102)).not_to eq 150 }
 end
